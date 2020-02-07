cac8a09978718a8c792ffc8f06e160330e236522
olf42 <olf@subsignal.org>
2020-02-07 15:45:36 +0100

    remove unuses path arg for get_db()

--- kescher/database.py
+++ kescher/database.py
@@ -2,9 +2,5 @@ from peewee import SqliteDatabase
 
 KESCHER_DB_NAME = "kescher.db"
 
-
-def get_db(path=None):
-    if not path:
-        return SqliteDatabase(KESCHER_DB_NAME)
-    else:
-        return SqliteDatabase(path / KESCHER_DB_NAME)
+def get_db():
+    return SqliteDatabase(KESCHER_DB_NAME)
